f59aa12d19ec6c68ec15c549cd797d34e4c092c4
Matthew Vine <32849887+MattTheCuber@users.noreply.github.com>
2023-11-02 11:28:42 -0400

    fix: broken import from batchgenerators

--- nnunet/training/data_augmentation/default_data_augmentation.py
+++ nnunet/training/data_augmentation/default_data_augmentation.py
@@ -15,7 +15,7 @@
 from copy import deepcopy
 
 import numpy as np
-from batchgenerators.dataloading import MultiThreadedAugmenter
+from batchgenerators.dataloading.multi_threaded_augmenter import MultiThreadedAugmenter
 from batchgenerators.transforms import DataChannelSelectionTransform, SegChannelSelectionTransform, SpatialTransform, \
     GammaTransform, MirrorTransform, Compose
 from batchgenerators.transforms.color_transforms import BrightnessMultiplicativeTransform, \
